22d3ff9b5ce6f9c6bad6929765ac50aad6bbae94
Purveshpund <purvesh.pund.work@gmail.com>
2021-09-01 04:34:13 +0530

    first Master commit with welcome msg

---
+++ EmpWageBuilderMain.java
@@ -0,0 +1,10 @@
+package assingmentEmp;
+
+public class EmpWageBuilderMain {
+
+	public static void main(String[] args) {
+		System .out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
+		System .out.println("Currently you are at Master Branch");
+	}
+
+}
